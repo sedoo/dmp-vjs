53104bb71d3666be891bb33e25f131d4e664b2ec
Thomas Romuald <thomas.romuald@obs-mip.fr>
2021-11-12 15:54:51 +0100

    use data-table for datasets

--- src/components/DataSets.vue
+++ src/components/DataSets.vue
@@ -5,136 +5,73 @@
     </h2>
    <v-container fluid>
 
-    <v-data-table :items="dataSets">
-    </v-data-table>
-
-    <v-data-iterator
-      :items="dataSets"
-      item-key="name"
-      :items-per-page="4"
-      hide-default-footer
-    >
-      <template v-slot:default="{ items }">
-        <v-row>
-          <v-col
-            v-for="item in items"
-            :key="item.title"
-            cols="12"
-            sm="6"
-            md="4"
-            lg="4"
-          >
-            <v-card>
-              <v-card-title>
-                <h4>{{ item.title }}</h4>
-                <div class="icons-right" v-if="item.dataset_id">
-                    <v-btn icon :href="item.dataset_id.identifier" target="_blank">
-                    <v-icon>mdi-link</v-icon>
-                    </v-btn>
-                </div>
-              </v-card-title>
-              <v-divider></v-divider>
-              <v-list>
-                <v-list-item>
-                  <v-list-item-content>Description:</v-list-item-content>
-                  <v-list-item-content class="align-end">
-                    {{ item.description }}
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-content>Personal data:</v-list-item-content>
-                  <v-list-item-content class="align-end">
-                    {{ item.personal_data }}
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-content>Sensitive data:</v-list-item-content>
-                  <v-list-item-content class="align-end">
-                    {{ item.sensitive_data }}
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-item v-if="item.type">
-                  <v-list-item-content>Type:</v-list-item-content>
-                  <v-list-item-content class="align-end">
-                    {{ item.type }}
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-group>
-                    <v-list-item slot='activator'>
-                        <v-list-item-title>Distribution</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item v-for="(subItem, y) in item.distribution"
-                        :key="y">
-                    <v-card class="ma-1" flat outlined>
-                    <v-list-item-content class="ml-2">
-                        <v-list-item-title>{{ subItem.title }}</v-list-item-title>
-                        <v-list-item-subtitle>
-                        URL: {{ subItem.access_url }}
-                        </v-list-item-subtitle>
-                        <v-list-item-subtitle>
-                        Data access: {{ subItem.data_access }}
-                        </v-list-item-subtitle>
-                        <v-list-item-subtitle v-if="subItem.byte_size">
-                        Volume: {{ bytesToSize(subItem.byte_size) }}
-                        </v-list-item-subtitle>
-                        <v-list-item-subtitle v-if="subItem.license">
-                        License:
-                            <a
-                                v-for="(link, i) in subItem.license"
-                                :key="i"
-                                :href="link.license_ref"
-                                class="subheading mx-3"
-                                target="_blank"
-                            >
-                                {{ link.license_name }}
-                            </a>
-                        </v-list-item-subtitle>
-                        <v-list-item-subtitle v-if="subItem.available_until">
-                            Available until: {{ subItem.available_until }}
-                        </v-list-item-subtitle>
-                        <template v-if="subItem.host">
+    <v-data-table 
+      :headers="headers" 
+      :items="dataSets" 
+      :expanded.sync="expanded"
+      single-expand
+      show-expand
+      item-key="title"
+      sort-by="title"
+      class="elevation-1">
+        <template v-slot:expanded-item="{ headers, item }">
+          <td :colspan="headers.length">
+            <template v-if="item.distribution != null">
+              <v-card flat outlined class="my-5">
+                <v-card-title>Distribution</v-card-title>
+                <v-card-text>
+                  <v-data-table 
+                    :headers="headersDistribution" 
+                    :items="item.distribution" 
+                    :expanded.sync="hostExpanded"
+                    single-expand
+                    show-expand
+                    item-key="title"
+                    hide-default-footer>
+                      <template v-slot:item.byte_size="{ item }">
+                        {{ bytesToSize(item.byte_size) }}
+                      </template> 
+                      <template v-slot:expanded-item="{ headers, item }">
+                        <td :colspan="headers.length">
+                        <template v-if="item.host">
                             <v-list-item-content>
-                                <v-list-item-title>Host {{ subItem.host.title  }}</v-list-item-title>
+                                <v-list-item-title>Host {{ item.host.title  }}</v-list-item-title>
                                 <v-list-item-subtitle class="wrap-text">
-                                Description: {{ subItem.host.description }}
+                                Description: {{ item.host.description }}
                                 </v-list-item-subtitle>
                                 <v-list-item-subtitle>
-                                URL: {{ subItem.host.url }}
+                                URL: {{ item.host.url }}
                                 </v-list-item-subtitle>
                                 <v-list-item-subtitle>
-                                Storage Type: {{ subItem.host.storage_type }}
+                                Storage Type: {{ item.host.storage_type }}
                                 </v-list-item-subtitle>
-                                <v-list-item-subtitle v-if="subItem.host.backup_frequency">
-                                Backup frequency: {{ subItem.host.backup_frequency }}
+                                <v-list-item-subtitle v-if="item.host.backup_frequency">
+                                Backup frequency: {{ item.host.backup_frequency }}
                                 </v-list-item-subtitle>
                             </v-list-item-content>
                         </template>
-                    </v-list-item-content>
-                    </v-card>
-                    </v-list-item>
-                </v-list-group>
-                <v-list-group v-if="item.technical_resource">
-                    <v-list-item slot='activator'>
-                        <v-list-item-title>Technical resources</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item v-for="(subItem, y) in item.technical_resource"
-                        :key="y">
-                    <v-card class="ma-1" flat outlined>
-                        <v-list-item-content class="ml-2">
-                            <v-list-item-title>{{ subItem.name }}</v-list-item-title>
-                            <v-list-item-subtitle class="wrap-text">
-                                <span v-html="subItem.description"></span>
-                            </v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-card>
-                    </v-list-item>
-                </v-list-group>
-              </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
-      </template>
-    </v-data-iterator>
+                        </td>
+                      </template>
+                  </v-data-table>
+                </v-card-text>
+              </v-card>
+              </template>
+            <template v-if="item.technical_resource != null">
+              <v-card flat outlined class="my-5">
+                <v-card-title>Technical resources</v-card-title>
+                <v-card-text>
+                  <v-data-table 
+                    :headers="headersTechResources" 
+                    :items="item.technical_resource" 
+                    hide-default-footer>
+                  </v-data-table>
+                </v-card-text>
+              </v-card>
+              </template>
+          </td>
+        </template>
+    </v-data-table>
+
   </v-container>
   </div>
 </template>
@@ -152,19 +89,36 @@
         },
     },
 
-    computed: {
-      headers: function() {
-        return [
-          { text: 'Nom', value: 'name' },
-          { text: 'Description', value: 'creationDate' },
-          { text: 'Personal data', value: 'latestValidReportUpdateDate' },
-          { text: this.$t('repository.table.column.report.inprogress.health'), value: 'latestInProgressReportUpdateDate' }
-          ]
-      } 
+    data() {
+      return {
+        headers: [],
+        headersDistribution: [],
+        expanded: [],
+        hostExpanded: [],
+      }
     },
 
     created: function() {
-        console.log(this.dataSets);
+      this.headers =  [
+          { text: 'Nom', value: 'title' },
+          { text: 'Description', value: 'description' },
+          { text: 'Personal data', value: 'personal_data' },
+          { text: 'Sensitive data', value: 'sensitive_data' },
+          { text: '', value: 'data-table-expand' }
+          ],
+      this.headersDistribution =  [
+          { text: 'Nom', value: 'title' },
+          { text: 'Description', value: 'description' },
+          { text: 'Download URL', value: 'download_url' },
+          { text: 'Data access', value: 'data_access' },
+          { text: 'Available until', value: 'available_until' },
+          { text: 'Volume', value: 'byte_size' },
+          { text: '', value: 'data-table-expand' }
+          ]
+      this.headersTechResources =  [
+          { text: 'Nom', value: 'name' },
+          { text: 'Description', value: 'description' }
+          ]
     },
 
     methods: {
